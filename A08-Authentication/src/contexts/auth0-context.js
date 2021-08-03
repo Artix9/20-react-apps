44311cce4cb71405018a4c73dcccb1f2eee7ee1a
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-04 01:40:16 +0900

    4 Adding Auth0 to Context

--- A08-Authentication/src/contexts/auth0-context.js
+++ A08-Authentication/src/contexts/auth0-context.js
@@ -1,10 +1,64 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import createAuth0Client from "@auth0/auth0-spa-js";
 
 export const Auth0Context = createContext();
 
 export function Auth0Provider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [auth0Client, setAuth0Client] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function initAuth0() {
+      const auth0 = await createAuth0Client({
+        domain: "dev-sebl5pdm.jp.auth0.com",
+        client_id: "KAyzB6yF8ohJ1WOkJzCezwmDNsVvSplb",
+        redirect_uri: window.location.origin,
+      });
+
+      setAuth0Client(auth0);
+
+      // handle redirect when user comes back
+      if (
+        window.location.search.includes("code=") &&
+        window.location.search.includes("state=")
+      ) {
+        try {
+          await auth0.handleRedirectCallback();
+        } catch (err) {
+          alert(err);
+        }
+
+        window.location.replace(window.location.pathname);
+      }
+
+      // is a user authenticated
+      const isAuthenticated = await auth0.isAuthenticated();
+      setIsAuthenticated(isAuthenticated);
+
+      // go grab the user
+      if (isAuthenticated) {
+        const user = await auth0.getUser();
+        setUser(user);
+      }
+
+      setIsLoading(false);
+    }
+
+    initAuth0();
+  }, []);
+
   return (
-    <Auth0Context.Provider value={{ name: "cool guy" }}>
+    <Auth0Context.Provider
+      value={{
+        isAuthenticated,
+        user,
+        isLoading,
+        login: (...p) => auth0Client.loginWithRedirect(...p),
+        logout: (...p) => auth0Client.logout(...p),
+      }}
+    >
       {children}
     </Auth0Context.Provider>
   );
