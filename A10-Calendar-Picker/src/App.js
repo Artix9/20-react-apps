2c09c915d52167a07be4150b878a8dda56634d3e
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-05 00:49:35 +0900

    1 Choosing a date

--- A10-Calendar-Picker/src/App.js
+++ A10-Calendar-Picker/src/App.js
@@ -1,25 +1,69 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 const calendarDates = Array(31)
   .fill(0)
   .map((e, i) => i);
 
 export default function App() {
+  const [choosingType, setChoosingType] = useState("start");
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  function updateDate(chosenDay) {
+    // handle if a user chose before our current range
+    if (startDate && chosenDay < startDate) {
+      setStartDate(chosenDay);
+      return setChoosingType("end");
+    }
+
+    // handle if a user chose after our current range
+    if (endDate && chosenDay > endDate) {
+      setEndDate(chosenDay);
+      return setChoosingType("end");
+    }
+
+    if (choosingType === "start") {
+      setStartDate(chosenDay);
+      setChoosingType("end");
+      return setChoosingType("end");
+    }
+
+    if (choosingType === "end") {
+      setEndDate(chosenDay);
+    }
+  }
+
   return (
     <>
       <div className="date-chooser">
-        <button className="date-chooser-button">
-          Start Date <span>0</span>
+        <button
+          className="date-chooser-button"
+          onClick={() => setChoosingType("start")}
+        >
+          Start Date <span>{startDate}</span>
         </button>
-        <button className="date-chooser-button">
-          End Date <span>0</span>
+        <button
+          className="date-chooser-button"
+          onClick={() => setChoosingType("end")}
+        >
+          End Date <span>{endDate}</span>
         </button>
       </div>
 
       <div className="calendar">
         {calendarDates.map((day, index) => {
-          return <div className="calendar-day">{day + 1}</div>;
+          const dayNumber = day + 1;
+
+          return (
+            <button
+              key={index}
+              className="calendar-day"
+              onClick={() => updateDate(dayNumber)}
+            >
+              {dayNumber}
+            </button>
+          );
         })}
       </div>
     </>
