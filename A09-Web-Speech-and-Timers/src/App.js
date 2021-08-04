4c8d50a25ed75105e03d09ea7618768ba3607092
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-04 17:57:09 +0900

    1 Creating timer forms

--- A09-Web-Speech-and-Timers/src/App.js
+++ A09-Web-Speech-and-Timers/src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [timers, setTimers] = useState([
+    { time: 2, text: "this is my message" },
+    { time: 5, text: "hello" },
+    { time: 8, text: "whats up" },
+  ]);
+
+  function updateTimers(index, time, text) {
+    const newTimers = [...timers];
+    newTimers[index].time = time;
+    newTimers[index].text = text;
+
+    setTimers(newTimers);
+  }
+
+  function addTimer() {
+    const newTimers = [...timers, { time: 100, text: "yooo" }];
+    setTimers(newTimers);
+  }
+
   return (
     <div className="app">
       <h2>Talk the Talk</h2>
 
       <div className="timers">
         {/* timers go here */}
-        <form className="timer">
-          <input type="number" />
-          <input type="text" />
-        </form>
+        {timers.map((timer, index) => (
+          <TimerSlot
+            key={index}
+            index={index}
+            timer={timer}
+            updateTimers={updateTimers}
+          />
+        ))}
 
-        <button className="add-button">Add</button>
+        <button className="add-button" onClick={addTimer}>
+          Add
+        </button>
       </div>
 
       {/* seconds */}
@@ -25,5 +51,31 @@ export default function App() {
         <button className="stop-button">Stop</button>
       </div>
     </div>
+  );
+}
+
+function TimerSlot({ index, timer, updateTimers }) {
+  const [time, setTime] = useState(timer.time);
+  const [text, setText] = useState(timer.text);
+
+  function handleBlur() {
+    updateTimers(index, time, text);
+  }
+
+  return (
+    <form className="timer" key={index}>
+      <input
+        type="number"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        onBlur={handleBlur}
+      />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onBlur={handleBlur}
+      />
+    </form>
   );
 }
