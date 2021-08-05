8caec3df182914ca7348cfd4942f1b39ee5f436e
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-05 19:57:08 +0900

    B01-1 Visualizing messages

--- B01-Animated-Chatbox/src/App.js
+++ B01-Animated-Chatbox/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import useInterval from "@use-it/interval";
 import "./App.css";
 
 const messages = [
@@ -12,17 +13,39 @@ const messages = [
 ];
 
 export default function App() {
+  const [messageToshow, setMessageToShow] = useState(0);
+
+  useInterval(() => {
+    setMessageToShow((messageToshow) => messageToshow + 1);
+  }, 2000);
+
   return (
     <div className="app">
       <div className="walkthrough">
         {messages.map((message, index) => {
-          return (
-            <div key={index} className="message">
-              {message.text}
-            </div>
-          );
+          // logic goes here
+
+          // are we supposed to show a typing indicator?
+          if (messageToshow + 1 === index) {
+            return <div key={index}>I am typing</div>;
+          }
+
+          // are we supposed to show this message?
+          if (index > messageToshow) return <div key={index} />;
+
+          return <Message key={index} message={message} />;
         })}
       </div>
+    </div>
+  );
+}
+
+function Message({ message }) {
+  return (
+    <div className="message">
+      <div className="avatar">🐶</div>
+      <div className="text">{message.text}</div>
+      <div className="avatar">🐱</div>
     </div>
   );
 }
