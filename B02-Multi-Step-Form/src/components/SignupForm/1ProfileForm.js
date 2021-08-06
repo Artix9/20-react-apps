90db5d12b7c17bbc3f88b062c400808f45e24f50
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-07 07:38:52 +0900

    B02-2 Forms and validation

--- B02-Multi-Step-Form/src/components/SignupForm/1ProfileForm.js
+++ B02-Multi-Step-Form/src/components/SignupForm/1ProfileForm.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router";
 
 export default function ProfileForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const history = useHistory();
+
+  function onSubmit(data) {
+    // do whatever we need here
+    console.log(data);
+    history.push("/social");
+  }
+
   return (
-    <form>
-      i am the profile form <input type="submit" value="Next" />
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <h2>Tell us about yourself</h2>
+
+      <input
+        type="text"
+        placeholder="What's your name?"
+        {...register("name", { required: true })}
+      />
+      {errors.name && <p>Name is required.</p>}
+
+      <input
+        type="text"
+        placeholder="What's your email?"
+        {...register("email", {
+          required: true,
+          pattern:
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        })}
+      />
+      {errors.email && <p>A valid email is required.</p>}
+
+      <input type="submit" value="Next" />
     </form>
   );
 }
--- B02-Multi-Step-Form/src/components/SignupForm/2SocialForm.js
+++ B02-Multi-Step-Form/src/components/SignupForm/2SocialForm.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
 export default function SocialForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const history = useHistory();
+
+  function onSubmit(data) {
+    // do whatever we need here
+    console.log(data);
+    history.push("/review");
+  }
+
   return (
-    <form>
-      i am the social form <input type="submit" value="Next" />
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+      <h2>How can we find you on social?</h2>
+
+      <input
+        type="text"
+        placeholder="What's your Twitter?"
+        {...register("twitter", { required: true })}
+      />
+      {errors.twitter && <p>Twitter is required.</p>}
+
+      <input
+        type="text"
+        placeholder="What's your Facebook?"
+        {...register("facebook", {
+          required: true,
+        })}
+      />
+      {errors.facebook && <p>Facebook is required.</p>}
+
+      <input type="submit" value="Next" />
     </form>
   );
 }
