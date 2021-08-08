0a216afda3a53aeb8cc3adfe41efaecaf76a5f73
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-08 21:55:28 +0900

    B03-1 Displaying Pokemon

--- B03-Memory-Matching-Game/src/App.js
+++ B03-Memory-Matching-Game/src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import './App.css';
-
-// image for the pokemon
-// https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png
+import React from "react";
+import shuffle from "lodash.shuffle";
+import "./App.css";
 
 const pokemon = [
-  { id: 4, name: 'charizard' },
-  { id: 10, name: 'caterpie' },
-  { id: 77, name: 'ponyta' },
-  { id: 108, name: 'lickitung' },
-  { id: 132, name: 'ditto' },
-  { id: 133, name: 'eevee' },
+  { id: 4, name: "charizard" },
+  { id: 10, name: "caterpie" },
+  { id: 77, name: "ponyta" },
+  { id: 108, name: "lickitung" },
+  { id: 132, name: "ditto" },
+  { id: 133, name: "eevee" },
 ];
+const doublePokemon = shuffle([...pokemon, ...pokemon]);
 
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="cards">
+        {doublePokemon.map((pokemon, index) => {
+          return <PokemonCard key={index} pokemon={pokemon} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function PokemonCard({ index, pokemon }) {
+  return (
+    <div className="pokemon-card flipped">
+      <div className="inner">
+        <div className="front">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+            alt={pokemon.name}
+            width="200"
+          />
+        </div>
+        <div className="back">?</div>
+      </div>
+    </div>
+  );
 }
