b9fa29dfb0615740f859ef05020542130bab18d9
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-09 23:47:20 +0900

    B04-3 Storing values in state

--- B04-Drag-and-Drop-Math-Card/src/App.js
+++ B04-Drag-and-Drop-Math-Card/src/App.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { DndProvider, useDrag, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import "./App.css";
 
 export default function App() {
+  const [number1, setNumber1] = useState(1);
+  const [number2, setNumber2] = useState(3);
+  const [operator, setOperator] = useState("*");
+
+  function handleDrop(spot, item) {
+    console.log(spot, item);
+    if (spot === "number1") setNumber1(item.text);
+    if (spot === "number2") setNumber2(item.text);
+    if (spot === "operator") setOperator(item.text);
+  }
+
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="app">
         {/* math card */}
         <div className="math-card">
-          <Spot type="number">1</Spot>
-          <Spot type="number">1</Spot>
-          <Spot type="operator">+</Spot>
-          <div className="total">2</div>
+          <Spot
+            type="number"
+            text={number1}
+            handleDrop={handleDrop}
+            spot="number1"
+          />
+          <Spot
+            type="number"
+            text={number2}
+            handleDrop={handleDrop}
+            spot="number2"
+          />
+          <Spot
+            type="operator"
+            text={operator}
+            handleDrop={handleDrop}
+            spot="operator"
+          />
+          <div className="total">{eval(`${number1}${operator}${number2}`)}</div>
         </div>
 
         <div>
@@ -20,13 +46,13 @@ export default function App() {
             {Array(10)
               .fill(0)
               .map((n, i) => (
-                <Number key={i} text={i} />
+                <Card key={i} type="number" text={i} />
               ))}
           </div>
 
           <div className="cards operators">
             {["*", "-", "+", "/"].map((o, i) => (
-              <Operator key={i} text={o} />
+              <Card key={i} type="operator" text={o} />
             ))}
           </div>
         </div>
@@ -35,11 +61,12 @@ export default function App() {
   );
 }
 
-function Spot({ type }) {
+function Spot({ type, text, spot, handleDrop }) {
   const [{ isOver, canDrop }, dropRef] = useDrop({
     accept: type,
     drop: (item) => {
-      console.log(type, item);
+      handleDrop(spot, item);
+      // here is where we do the update
     },
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
@@ -53,31 +80,15 @@ function Spot({ type }) {
 
   return (
     <div className="spot" ref={dropRef} style={{ backgroundColor }}>
-      0
-    </div>
-  );
-}
-
-function Number({ text }) {
-  const [{ opacity }, dragRef] = useDrag(() => ({
-    type: "number",
-    item: { number: text },
-    collect: (monitor) => ({
-      opacity: monitor.isDragging() ? 0.5 : 1,
-    }),
-  }));
-
-  return (
-    <div className="card" ref={dragRef} style={{ opacity }}>
       {text}
     </div>
   );
 }
 
-function Operator({ text }) {
+function Card({ type, text }) {
   const [{ opacity }, dragRef] = useDrag(() => ({
-    type: "operator",
-    item: { operator: text },
+    type: type,
+    item: { text },
     collect: (monitor) => ({
       opacity: monitor.isDragging() ? 0.5 : 1,
     }),
