00dd8f1adc8e07aa42be6f736151adb66811df02
Artix9 <xxxxx01xxxxx@naver.com>
2021-08-14 21:42:28 +0900

    B09-1 Showing characters

--- B09-Typing-Test/src/App.js
+++ B09-Typing-Test/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 // to calculate typing speed
 // words typed / minutes
@@ -9,6 +9,8 @@ const secondsToCount = 10;
 const paragraph = `Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
 
 export default function App() {
+  const [typedText, setTypedText] = useState("");
+
   return (
     <div className="app">
       {/* sidebar */}
@@ -20,11 +22,34 @@ export default function App() {
 
       <div className="content">
         {/* show the paragraph */}
-        <p>{paragraph}</p>
+        <p>
+          {paragraph.split("").map((character, index) => {
+            // check what class to apply to each character
+            let characterClass = "";
+            const hasBeenTyped = typedText.length > index;
+
+            if (hasBeenTyped) {
+              let typo = true;
+              if (typo) characterClass = "incorrect";
+              if (!typo) characterClass = "correct";
+            }
+
+            return (
+              <span key={index} className={characterClass}>
+                {character}
+              </span>
+            );
+          })}
+        </p>
 
         {/* show the textarea */}
         <form>
-          <textarea rows="10" placeholder="Test your typing skills..." />
+          <textarea
+            value={typedText}
+            onChange={(e) => setTypedText(e.target.value)}
+            rows="10"
+            placeholder="Test your typing skills..."
+          />
         </form>
       </div>
     </div>
